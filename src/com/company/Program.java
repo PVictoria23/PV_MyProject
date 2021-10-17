97aff4c29b210bd6695e856f5c1c03283e4d6000
PVicoria23 <pvv230882@gmail.com>
2021-10-17 20:33:32 +0300

    First commit

---
+++ src/com/company/Program.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        int x=10;
+        System.out.println(x);
+        int a = 10, y = 30;
+        System.out.println(a);
+        System.out.println(y);
+        int d =15;
+        System.out.println(d);
+        int t=35;
+        System.out.println(35);
+
+
+
+
+    }
+}
+
+
+
+
